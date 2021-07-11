412701194d6300cb370465c32e21f9d4295a44a9
priyanka455 <83944935+priyanka455@users.noreply.github.com>
2021-07-11 20:50:06 +0530

    initial commit

---
+++ src/app/admin/screen-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-screen-view',
+  templateUrl: './screen-view.component.html',
+  styleUrls: ['./screen-view.component.css']
+})
+export class ScreenViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
